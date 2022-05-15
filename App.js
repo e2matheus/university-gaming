3e11ed77e4f4ddff01afaf947e7a68d88805275c
Eleazar Matheus <e2matheus@gmail.com>
2022-05-15 09:31:42 +0200

    Add universities wrapper

--- App.js
+++ App.js
@@ -4,7 +4,12 @@ import { StyleSheet, Text, View } from 'react-native';
 const App = () => {
   return (
         <View style={styles.container} >
-          <Text>University Gaming</Text>
+          <View style={styles.universitiesWrapper}>
+            <Text style={styles.sectionTitle}>Today's universities</Text>
+            <View style={styles.items}>
+              {/* Where the universities will go */}
+            </View>
+          </View>
         </View>
   );
 };
@@ -12,10 +17,17 @@ const App = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#E8EAED',
   },
+  universitiesWrapper: {
+    paddingTop: 80,
+    paddingHorizontal: 20,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  items: {},
 });
 
 export default App;
