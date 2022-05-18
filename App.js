18eadf942721721be774c35d92753f038312f34f
Eleazar Matheus <e2matheus@gmail.com>
2022-05-18 14:12:57 +0200

    Show university and details card components on Home screen

--- App.js
+++ App.js
@@ -1,18 +1,38 @@
 import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
-import UniversityDetails from './src/components/UniversityDetails/UniversityDetails';
+import University from './src/components/University';
+import UniversityDetailsCard from './src/components/UniversityDetailsCard/UniversityDetailsCard';
 
 const App = () => {
-  const title = "Sevilla University";
-
   return (
-        <UniversityDetails
-          title={title}
-          name={title}
-          country="Spain"
-          aboutGaming="Unreal 5 Course"
-        />
+        <View style={styles.container} >
+          <View style={styles.universitiesWrapper}>
+            <Text style={styles.sectionTitle}>Today's universities</Text>
+            <View style={styles.items}>
+              <University title="University 1" />
+              <University title="University 2" />
+              <UniversityDetailsCard name="Sevilla University" country="Spain" aboutGaming="Unreal 5 Course" />
+            </View>
+          </View>
+        </View>
   );
 };
-
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#E8EAED',
+  },
+  universitiesWrapper: {
+    paddingTop: 80,
+    paddingHorizontal: 20,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  items: {
+    marginTop: 30,
+  },
+});
 export default App;
