5ae63d47d825ea7c6489760d3d040ec965bda834
Eleazar Matheus <e2matheus@gmail.com>
2022-05-25 11:56:08 +0200

    Add and use missing navigation library
    * Install react navigation native library
    
    * Import library on app component and wrap main view in navigation container

--- App.js
+++ App.js
@@ -1,21 +1,24 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 
 import University from './src/components/University';
 import UniversityDetailsCard from './src/components/UniversityDetailsCard/UniversityDetailsCard';
 
 const App = () => {
   return (
-        <View style={styles.container} >
-          <View style={styles.universitiesWrapper}>
-            <Text style={styles.sectionTitle}>Today's universities</Text>
-            <View style={styles.items}>
-              <University title="University 1" />
-              <University title="University 2" />
-              <UniversityDetailsCard name="Sevilla University" country="Spain" aboutGaming="Unreal 5 Course" />
-            </View>
+    <NavigationContainer>
+      <View style={styles.container} >
+        <View style={styles.universitiesWrapper}>
+          <Text style={styles.sectionTitle}>Today's universities</Text>
+          <View style={styles.items}>
+            <University title="University 1" />
+            <University title="University 2" />
+            <UniversityDetailsCard name="Sevilla University" country="Spain" aboutGaming="Unreal 5 Course" />
           </View>
         </View>
+      </View>
+    </NavigationContainer>
   );
 };
 const styles = StyleSheet.create({
